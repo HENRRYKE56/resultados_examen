597192dd2df60506cb4c684bcf66597a187a2d77
Henry Landeros <87285887+HENRRYKE56@users.noreply.github.com>
2025-12-12 12:00:54 -0600

    nombre

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -20,7 +20,7 @@
   <body class="hold-transition login-page">
     <div class="login-box">
       <div class="login-logo">
-        <a href="#"><b>SEIEM</b><br>Sistema de Gestión</a>
+        <a href="#"><b>SEIEM</b><br>Resultados de Evalaución de Conocimientos</a>
       </div><!-- /.login-logo -->
       <div class="login-box-body">
         <p class="login-box-msg">Iniciar Sesión</p>
